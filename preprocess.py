74f69840527df6b7a5bdc0d0869801091336a027
myoons <grandchasevs@kaist.ac.kr>
2021-02-01 16:08:45 +0900

    Make as Module

--- preprocess.py
+++ preprocess.py
@@ -7,138 +7,138 @@ from tqdm import tqdm
 from transformers import ElectraTokenizer
 
 
-if os.path.isfile('nsmc.h5'):
-    print("H5 File already exists")
-else:
-    train_dataset = pd.read_csv('data/ratings_train.txt', sep='\t').dropna(axis=0)  # Remove NaN
-    test_dataset = pd.read_csv('data/ratings_test.txt', sep='\t').dropna(axis=0)  # Remove NaN
+def preprocess(args):
 
-    train_dataset.drop_duplicates(subset=['document'], inplace=True)  # Remove Duplicates
-    test_dataset.drop_duplicates(subset=['document'], inplace=True)  # Remove Duplicates
+    if os.path.isfile('nsmc.h5'):
+        print("H5 File already exists")
+    else:
+        train_dataset = pd.read_csv(f'{args.data_root}/ratings_train.txt', sep='\t').dropna(axis=0)  # Remove NaN
+        test_dataset = pd.read_csv(f'{args.data_root}/ratings_test.txt', sep='\t').dropna(axis=0)  # Remove NaN
 
-    print(f"Train Set Size : {len(train_dataset)}")
-    print(f"Test Set Size : {len(test_dataset)}")
+        train_dataset.drop_duplicates(subset=['document'], inplace=True)  # Remove Duplicates
+        test_dataset.drop_duplicates(subset=['document'], inplace=True)  # Remove Duplicates
 
-    tokenizer = ElectraTokenizer.from_pretrained("monologg/koelectra-base-v3-discriminator")
-    
-    nsmc = h5py.File('data/nsmc.h5', 'w', rdcc_nslots=11213, rdcc_nbytes=1024**3, rdcc_w0=1)
-    
-    ######################################################################################################
-    ######################################### Train Dataset ##############################################
-    ######################################################################################################
+        print(f"Train Set Size : {len(train_dataset)}")
+        print(f"Test Set Size : {len(test_dataset)}")
 
-    list_input_ids = []
-    list_attention_mask = []
-    list_label = []
+        tokenizer = ElectraTokenizer.from_pretrained("monologg/koelectra-base-v3-discriminator")
 
-    for i in tqdm(range(len(train_dataset)), total=len(train_dataset), desc="Parsing Training Datasets"):
+        nsmc = h5py.File(f'{args.data_root}/nsmc.h5', 'w', rdcc_nslots=11213, rdcc_nbytes=1024**3, rdcc_w0=1)
 
-        text, label = train_dataset.iloc[i, 1:3].values
+        ######################################################################################################
+        ######################################### Train Dataset ##############################################
+        ######################################################################################################
 
-        inputs = tokenizer(
-                text, 
-                return_tensors='pt',
-                truncation=True,
-                max_length=180,
-                pad_to_max_length=True,
-                add_special_tokens=True
-                )
+        list_input_ids = []
+        list_attention_mask = []
+        list_label = []
 
+        for i in tqdm(range(len(train_dataset)), total=len(train_dataset), desc="Parsing Training Datasets"):
 
-        input_ids = inputs['input_ids'][0]  # LongTensor / Size : 180
-        attention_mask = inputs['attention_mask'][0]  # LongTensor / Size : 180
+            text, label = train_dataset.iloc[i, 1:3].values
 
-        list_input_ids.append(input_ids.numpy())
-        list_attention_mask.append(attention_mask.numpy())
-        list_label.append(label)
+            inputs = tokenizer(
+                    text,
+                    return_tensors='pt',
+                    truncation=True,
+                    max_length=180,
+                    pad_to_max_length=True,
+                    add_special_tokens=True
+                    )
 
-    
-    ndarray_input_ids = np.array(list_input_ids)
-    ndarray_attention_mask = np.array(list_attention_mask)
-    ndarray_label = np.array(list_label)
+            input_ids = inputs['input_ids'][0]  # LongTensor / Size : 180
+            attention_mask = inputs['attention_mask'][0]  # LongTensor / Size : 180
 
-    print("Training Dataset Summary")
-    print(f"input_ids : {ndarray_input_ids.shape} / {ndarray_input_ids.dtype}")
-    print(f"attention_mask : {ndarray_attention_mask.shape} / {ndarray_attention_mask.dtype}")
-    print(f"label : {ndarray_label.shape} / {ndarray_label.dtype}")
+            list_input_ids.append(input_ids.numpy())
+            list_attention_mask.append(attention_mask.numpy())
+            list_label.append(label)
 
-    nsmc_h5_train_grp = nsmc.create_group('train')
+        ndarray_input_ids = np.array(list_input_ids)
+        ndarray_attention_mask = np.array(list_attention_mask)
+        ndarray_label = np.array(list_label)
 
-    nsmc_h5_train_grp.create_dataset('input_ids',
-                        data=ndarray_input_ids,
-                        dtype=np.long,
-                        chunks=(1000, 180),
-                        maxshape=(146182, 180))
+        print("Training Dataset Summary")
+        print(f"input_ids : {ndarray_input_ids.shape} / {ndarray_input_ids.dtype}")
+        print(f"attention_mask : {ndarray_attention_mask.shape} / {ndarray_attention_mask.dtype}")
+        print(f"label : {ndarray_label.shape} / {ndarray_label.dtype}")
 
-    nsmc_h5_train_grp.create_dataset('attention_mask',
-                        data=ndarray_attention_mask,
-                        dtype=np.long,
-                        chunks=(1000, 180),
-                        maxshape=(146182, 180))
+        nsmc_h5_train_grp = nsmc.create_group('train')
 
-    nsmc_h5_train_grp.create_dataset('label',
-                        data=ndarray_label,
-                        dtype=np.long,
-                        chunks=(1000,),  # 11 MB : Chunk Size
-                        maxshape=(146182,))      
+        nsmc_h5_train_grp.create_dataset('input_ids',
+                            data=ndarray_input_ids,
+                            dtype=np.long,
+                            chunks=(1000, 180),
+                            maxshape=(146182, 180))
 
+        nsmc_h5_train_grp.create_dataset('attention_mask',
+                            data=ndarray_attention_mask,
+                            dtype=np.long,
+                            chunks=(1000, 180),
+                            maxshape=(146182, 180))
 
-    ######################################################################################################
-    ######################################### Test Dataset ##############################################
-    ######################################################################################################
-
-    list_input_ids = []
-    list_attention_mask = []
-    list_label = []
-
-    for i in tqdm(range(len(test_dataset)), total=len(test_dataset), desc="Parsing Test Datasets"):
-
-        text, label = test_dataset.iloc[i, 1:3].values
-
-        inputs = tokenizer(
-                text, 
-                return_tensors='pt',
-                truncation=True,
-                max_length=180,
-                pad_to_max_length=True,
-                add_special_tokens=True
-                )
+        nsmc_h5_train_grp.create_dataset('label',
+                            data=ndarray_label,
+                            dtype=np.long,
+                            chunks=(1000,),  # 11 MB : Chunk Size
+                            maxshape=(146182,))
 
 
-        input_ids = inputs['input_ids'][0]  # LongTensor / Size : 180
-        attention_mask = inputs['attention_mask'][0]  # LongTensor / Size : 180
+        ######################################################################################################
+        ######################################### Test Dataset ##############################################
+        ######################################################################################################
 
-        list_input_ids.append(input_ids.numpy())
-        list_attention_mask.append(attention_mask.numpy())
-        list_label.append(label)
+        list_input_ids = []
+        list_attention_mask = []
+        list_label = []
 
-    
-    ndarray_input_ids = np.array(list_input_ids)
-    ndarray_attention_mask = np.array(list_attention_mask)
-    ndarray_label = np.array(list_label)
+        for i in tqdm(range(len(test_dataset)), total=len(test_dataset), desc="Parsing Test Datasets"):
 
-    print("Test Dataset Summary")
-    print(f"input_ids : {ndarray_input_ids.shape} / {ndarray_input_ids.dtype}")
-    print(f"attention_mask : {ndarray_attention_mask.shape} / {ndarray_attention_mask.dtype}")
-    print(f"label : {ndarray_label.shape} / {ndarray_label.dtype}")
+            text, label = test_dataset.iloc[i, 1:3].values
 
-    nsmc_h5_test_grp = nsmc.create_group('test')
+            inputs = tokenizer(
+                    text,
+                    return_tensors='pt',
+                    truncation=True,
+                    max_length=180,
+                    pad_to_max_length=True,
+                    add_special_tokens=True
+                    )
 
-    nsmc_h5_test_grp.create_dataset('input_ids',
-                        data=ndarray_input_ids,
-                        dtype=np.long,
-                        chunks=(1000, 180),
-                        maxshape=(49157, 180))
 
-    nsmc_h5_test_grp.create_dataset('attention_mask',
-                        data=ndarray_attention_mask,
-                        dtype=np.long,
-                        chunks=(1000, 180),
-                        maxshape=(49157, 180))
+            input_ids = inputs['input_ids'][0]  # LongTensor / Size : 180
+            attention_mask = inputs['attention_mask'][0]  # LongTensor / Size : 180
 
-    nsmc_h5_test_grp.create_dataset('label',
-                        data=ndarray_label,
-                        dtype=np.long,
-                        chunks=(1000,),
-                        maxshape=(49157,))              
+            list_input_ids.append(input_ids.numpy())
+            list_attention_mask.append(attention_mask.numpy())
+            list_label.append(label)
+
+
+        ndarray_input_ids = np.array(list_input_ids)
+        ndarray_attention_mask = np.array(list_attention_mask)
+        ndarray_label = np.array(list_label)
+
+        print("Test Dataset Summary")
+        print(f"input_ids : {ndarray_input_ids.shape} / {ndarray_input_ids.dtype}")
+        print(f"attention_mask : {ndarray_attention_mask.shape} / {ndarray_attention_mask.dtype}")
+        print(f"label : {ndarray_label.shape} / {ndarray_label.dtype}")
+
+        nsmc_h5_test_grp = nsmc.create_group('test')
+
+        nsmc_h5_test_grp.create_dataset('input_ids',
+                            data=ndarray_input_ids,
+                            dtype=np.long,
+                            chunks=(1000, 180),
+                            maxshape=(49157, 180))
+
+        nsmc_h5_test_grp.create_dataset('attention_mask',
+                            data=ndarray_attention_mask,
+                            dtype=np.long,
+                            chunks=(1000, 180),
+                            maxshape=(49157, 180))
+
+        nsmc_h5_test_grp.create_dataset('label',
+                            data=ndarray_label,
+                            dtype=np.long,
+                            chunks=(1000,),
+                            maxshape=(49157,))
     
